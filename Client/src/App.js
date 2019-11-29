f6b0fafb6b3b8a296199eb5389f2d5f4eaf56ce7
JnecUA <woodoomax123@gmail.com>
2019-11-29 22:44:05 +0300

    Setup links

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header'
+import Header from './components/Header';
+import Login from './pages/Login';
+import Main from './pages/Main';
+import Register from './pages/Register';
+import Support from './pages/Support';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <div className="bg"></div>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route path="/login" component={Login}/>
+          <Route path="/register" component={Register}/>
+          <Route path="/support" component={Support}/>
+        </Switch>
+        
+      </div>
+    </Router>
   );
 }
 
