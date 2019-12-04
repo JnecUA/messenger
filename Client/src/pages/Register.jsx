c6d20747352cc1f94848f4795a3a7747522f4859
JnecUA <woodoomax123@gmail.com>
2019-12-04 22:09:15 +0300

    Customize registration form

--- Client/src/pages/Register.jsx
+++ Client/src/pages/Register.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import './Register.css'
 
 const Register = () => (
     <div className="register">
         <form>
-                <input type="email" placeholder="Почта" />
-                <input type="password" placeholder="Пароль" />
-                <input type="password" placeholder="Подтверждение"/>
+                <div className="form-section">
+                    <input type="text" name="email" className="email-input" />
+                    <label htmlFor="email" className="email-label"><span>Почта</span></label>
+                </div>
+                <div className="form-section">
+                    <input type="text" name="name" className="name-input" />
+                    <label htmlFor="name" className="name-label"><span>Имя</span></label>
+                </div>
+                <div className="form-section">
+                    <input type="text" name="pass" className="pass-input" />
+                    <label htmlFor="pass" className="pass-label"><span>Пароль</span></label>
+                </div>
+                <div className="form-section">
+                    <input type="text" name="pass-confirm" className="pass-confirm-input" />
+                    <label htmlFor="pass-confirm" className="pass-confirm-label"><span>Подтверждение</span></label>
+                </div>
                 <input type="button" value="Зарегестрироваться"/>
             </form>
     </div>
