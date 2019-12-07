bd1f04a71df657c62af8123594624fa5c18fdda5
JnecUA <woodoomax123@gmail.com>
2019-12-07 14:58:34 +0300

    Setting up the login and registration form

--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import './Login.css'
+import './Login.css';
 
 const Login = () => {
     return(
         <div className="login">
             <form>
                 <section className="form-section">
-                    <input type="email" name="email" className="email-input"/>
+                    <input type="email" name="email" className="email-input" />
                     <label htmlFor="email" type="text" className="email-label"><span>Почта или логин</span></label>
                 </section>
                 <section className="form-section">
-                    <input type="password" name="password" className="pass-input" />
+                    <input type="password" name="password" className="pass-input" required ></input>
                     <label htmlFor="password" type="text" className="pass-label"><span>Пароль</span></label>
                 </section>
-                <input type="submit" value="Войти" />
+                <input type="submit" value="Войти" className="form-btn" />
 
             </form>
         </div>
--- Client/src/pages/Register.jsx
+++ Client/src/pages/Register.jsx
@@ -5,22 +5,22 @@ const Register = () => (
     <div className="register">
         <form>
                 <div className="form-section">
-                    <input type="text" name="email" className="email-input" />
+                    <input type="text" name="email" className="email-input-reg" required />
                     <label htmlFor="email" className="email-label"><span>Почта</span></label>
                 </div>
                 <div className="form-section">
-                    <input type="text" name="name" className="name-input" />
+                    <input type="text" name="name" className="name-input" required />
                     <label htmlFor="name" className="name-label"><span>Имя</span></label>
                 </div>
                 <div className="form-section">
-                    <input type="text" name="pass" className="pass-input" />
+                    <input type="text" name="pass" className="pass-input" required />
                     <label htmlFor="pass" className="pass-label"><span>Пароль</span></label>
                 </div>
                 <div className="form-section">
-                    <input type="text" name="pass-confirm" className="pass-confirm-input" />
+                    <input type="text" name="pass-confirm" className="pass-confirm-input" required />
                     <label htmlFor="pass-confirm" className="pass-confirm-label"><span>Подтверждение</span></label>
                 </div>
-                <input type="button" value="Зарегестрироваться"/>
+                <input type="button" value="Зарегестрироваться" className="form-btn"/>
             </form>
     </div>
 );
